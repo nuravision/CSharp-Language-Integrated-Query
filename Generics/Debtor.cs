cec80941d26e417182b6b77ce596abfc6f8aeb0d
nuravision <nurane.ismayilzade19@gmail.com>
2024-12-27 15:25:58 +0400

    Add project files.

---
+++ Generics/Debtor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Debtor:BaseEntity
+    {
+        public string FullName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public int Debt { get; set; }
+        public int _id { get; set; }
+        public Debtor(string fullname, DateTime birthDay, string phone, string email, string address, int debt)
+        {
+            _id = ++Id;
+            this.FullName = fullname;
+            this.BirthDay = birthDay;
+            this.Phone = phone;
+            this.Email = email;
+            this.Address = address;
+            this.Debt = debt;
+        }
+        public override string ToString()
+        {
+            return $"{this._id} {this.FullName} {this.BirthDay.ToShortDateString()} {this.Phone} {this.Email} {this.Address} {this.Debt}";
+        }
+
+
+    }
+}
